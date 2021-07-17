b3484ccf1f684570f67c44ef3324ba372266c2f2
EC2 Default User <ec2-user@ip-172-31-88-82.ec2.internal>
2021-07-17 09:16:51 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,44 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+
+
+
+  resources :customers, only: [:show, :edit, :update] do
+    member do
+      get :unsubscribe
+      patch :withdraw
+    end
+  end
+
+  resources :items, only: [:index, :show]
+  resources :cart_items, only: [:index, :create, :update, :destroy] do
+     member do
+      delete :destroy_all
+    end
+  end
+  resources :orders, only: [:new, :show, :create, :index] do
+   member do
+      post :comfirm
+      get :complete
+    end
+  end
+  resources :addresses, only: [:index, :edit, :create, :update,:destroy]
+
+  namespace:admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+    resources :genres, only: [:create, :index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+end
---
+++ db/migrate/20210717075800_add_columns_to_customers.rb
@@ -0,0 +1,10 @@
+class AddColumnsToCustomers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :customers, :last_name, :string
+    add_column :customers, :first_name, :string
+    add_column :customers, :postal_code, :string
+    add_column :customers, :address, :string
+    add_column :customers, :phone_number, :string
+    add_column :customers, :is_deleted, :boolean
+  end
+end
