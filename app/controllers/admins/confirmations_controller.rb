8ff9c9d86ba0b44d289c569c22b063593929ed2d
EC2 Default User <ec2-user@ip-172-31-88-82.ec2.internal>
2021-07-17 10:02:16 +0000

    [コントローラー追加]

--- app/controllers/admins/confirmations_controller.rb
+++ app/controllers/admins/confirmations_controller.rb
@@ -27,4 +27,5 @@ class Admins::ConfirmationsController < Devise::ConfirmationsController
   # def after_confirmation_path_for(resource_name, resource)
   #   super(resource_name, resource)
   # end
+  
 end
