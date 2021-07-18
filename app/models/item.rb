b36cbc3e341c047e44752981ab994de086a076ff
EC2 Default User <ec2-user@ip-172-31-88-82.ec2.internal>
2021-07-18 09:04:48 +0000

    [add 商品投稿　ジャンル追加]

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
-    
-  belongs_to :genres
+
+  belongs_to :genre
+  attachment :image
 end
