8e206a228737b3da1cab482c763dc4cff9721af5
kchoco <kyoka.n.0127@gmail.com>
2021-07-19 11:56:24 +0000

    退会処理追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -17,8 +17,16 @@ class Public::CustomersController < ApplicationController
     end
     end
     
-    def unsubscrib
+    def unsubscribe
+    @customer = current_customer
     end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_valid: false)
+    reset_session
+    redirect_to root_path
+  end
     
     private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_042450) do
+ActiveRecord::Schema.define(version: 2021_07_19_103932) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_07_18_042450) do
     t.boolean "is_deleted"
     t.string "last_name_kana"
     t.string "first_name_kana"
+    t.boolean "is_valid", default: true, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
