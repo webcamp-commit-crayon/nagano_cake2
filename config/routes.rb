9e7ccf1dd6ae4a862020b9458114fe3d3a074d19
kztn0112 <ngwn1995@gmail.com>
2021-07-24 11:17:20 +0000

    [修正]注文履歴一覧

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Rails.application.routes.draw do
     resources :customers, only: [:index, :show, :edit, :update]
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :genres, only: [:create, :index, :show, :edit, :update]
-    resources :orders, only: [:index]
+    resources :orders, only: [:show]
     resources :order_details, only: [:update]
   end
 end
