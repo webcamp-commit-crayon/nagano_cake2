2b65eead782a08ed7ce46d4601ab7251de1c759f
hirokiito0 <syu.itou.h@gamil.com>
2021-07-25 03:07:44 +0000

    genres_controllerコード整理

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,6 +1,6 @@
 class Admin::GenresController < ApplicationController
   def index
-    @genre=Genre.new
+    @genre  = Genre.new
     @genres = Genre.all
   end
 
@@ -9,10 +9,10 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
-    @genre = Genre.new(genre_params)
+    @genre  = Genre.new(genre_params)
     @genres = Genre.all
     if @genre.save
-      flash[:info] = '登録に成功しました。'
+      flash[:info]       = '登録に成功しました。'
       redirect_to admin_genres_path
     else
       flash.now[:danger] = '登録に失敗しました。'
@@ -23,7 +23,7 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
-      flash[:info] = '登録に成功しました。'
+      flash[:info]       = '登録に成功しました。'
       redirect_to admin_genres_path
     else
       flash.now[:danger] = '登録に失敗しました。'
