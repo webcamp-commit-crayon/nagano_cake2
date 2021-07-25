cafc7b6e9b110102662ae1a7bfa1506508f760c2
EC2 Default User <ec2-user@ip-172-31-80-72.ec2.internal>
2021-07-25 06:08:27 +0000

    orders_controllerコード調整

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,18 +1,18 @@
 class Public::OrdersController < ApplicationController
     
     def new
-        @orders = Order.all
+        @orders    = Order.all
         @addresses = Address.all
     end
     
     def comfirm
-        @cart_items = current_customer.cart_items
-        @total = 0
+        @cart_items  = current_customer.cart_items
+        @total       = 0
       @cart_items.each do |cart_item| 
         total_amount = (cart_item.item.price * cart_item.amount*1.1).round
-        @total += total_amount
+        @total      += total_amount
       end
-      session[:customer] = current_customer.orders.build
+      session[:customer]                 = current_customer.orders.build
       session[:customer][:total_payment] = @total+800
        if params[:payment_select] == "1"
          session[:customer][:payment_method] = 1
@@ -22,32 +22,32 @@ class Public::OrdersController < ApplicationController
     
        if params[:address_select] == "0"
          session[:customer][:postal_code] = current_customer.postal_code
-         session[:customer][:address] = current_customer.address
-         session[:customer][:name] = current_customer.last_name
+         session[:customer][:address]     = current_customer.address
+         session[:customer][:name]        = current_customer.last_name
        elsif params[:address_select] == "1"
          session[:customer][:postal_code] = Address.find(params[:address_id]).postal_code
-         session[:customer][:address] = Address.find(params[:address_id]).address
-         session[:customer][:name] = Address.find(params[:address_id]).name
+         session[:customer][:address]     = Address.find(params[:address_id]).address
+         session[:customer][:name]        = Address.find(params[:address_id]).name
        else 
          session[:customer][:postal_code] = params[:postal_code]
-         session[:customer][:address] = params[:address]
-         session[:customer][:name] = params[:name]
+         session[:customer][:address]     = params[:address]
+         session[:customer][:name]        = params[:name]
        end
     end
     
     def create
-         @order = Order.new(session[:customer])
-         @order.customer_id = current_customer.id
-         @cart_items = current_customer.cart_items
+         @order               = Order.new(session[:customer])
+         @order.customer_id   = current_customer.id
+         @cart_items          = current_customer.cart_items
          @order.shipping_cost = 800
          @order.save
          @cart_items.each do |cart_item|
-             @order_detail = OrderDetail.new
-             @order_detail.item_id = cart_item.item_id
+             @order_detail               = OrderDetail.new
+             @order_detail.item_id       = cart_item.item_id
              @order_detail.making_status = 0
-             @order_detail.price = cart_item.item.price
-             @order_detail.amount= cart_item.amount
-             @order_detail.order_id = @order.id
+             @order_detail.price         = cart_item.item.price
+             @order_detail.amount        = cart_item.amount
+             @order_detail.order_id      = @order.id
              @order_detail.save
           end
         current_customer.cart_items.destroy_all
@@ -63,7 +63,7 @@ class Public::OrdersController < ApplicationController
         @order = Order.find(params[:id])
         @total = 0
           @order.order_details.each do |order_detail|
-          @total += order_detail.price*order_detail.amount
+            @total += order_detail.price*order_detail.amount
           end 
     end
 
